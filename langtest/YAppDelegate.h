ad29b1b3c50fc46bb6bf4f0f9d5a67708ee6f225
Bryn Austin Bellomy <bryn@signals.io>
2012-08-29 02:59:16 -0400

    Initial Commit

---
+++ langtest/YAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YAppDelegate.h
+//  langtest
+//
+//  Created by bryn austin bellomy on 8.29.12.
+//  Copyright (c) 2012 robot bubble bath LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface YAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ langtest/YAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  YAppDelegate.m
+//  langtest
+//
+//  Created by bryn austin bellomy on 8.29.12.
+//  Copyright (c) 2012 robot bubble bath LLC. All rights reserved.
+//
+
+#import "YAppDelegate.h"
+
+@implementation YAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
